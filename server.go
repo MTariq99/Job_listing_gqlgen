857ca3ec4083b7bfe8ac537eaadfe05007f5d553
MTariq <tariqwc99@gmail.com>
2024-08-10 23:21:02 +0500

    added configuration

--- server.go
+++ server.go
@@ -3,26 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/mtariq99/graphqlexample/config"
 	"github.com/mtariq99/graphqlexample/graph"
 )
 
-const defaultPort = "8080"
+func init() {
+	if err := config.LoadConfig(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
 	Resolver := graph.NewResolver()
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: Resolver}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", config.Cfg.ServerPort)
+	log.Fatal(http.ListenAndServe(":"+config.Cfg.ServerPort, nil))
 }
